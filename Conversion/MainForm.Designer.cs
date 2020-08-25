21ff3b198a5c6bc79e131347083744bb03ffd4a6
VeremyevVadim <qwertyvadimv@mail.ru>
2020-08-25 16:59:39 +0300

    Добавьте файлы проекта.

---
+++ Conversion/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Conversion
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReconnect = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.cbDestCurrency = new System.Windows.Forms.ComboBox();
+            this.cbSourceCurrency = new System.Windows.Forms.ComboBox();
+            this.tbDestCurrency = new System.Windows.Forms.TextBox();
+            this.tbSourceCurrency = new System.Windows.Forms.TextBox();
+            this.lblConnected = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnReconnect
+            // 
+            this.btnReconnect.Location = new System.Drawing.Point(15, 25);
+            this.btnReconnect.Name = "btnReconnect";
+            this.btnReconnect.Size = new System.Drawing.Size(137, 23);
+            this.btnReconnect.TabIndex = 0;
+            this.btnReconnect.Text = "Переподключиться";
+            this.btnReconnect.UseVisualStyleBackColor = true;
+            this.btnReconnect.Click += new System.EventHandler(this.btnReconnect_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Enabled = false;
+            this.btnConvert.Location = new System.Drawing.Point(205, 139);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(105, 23);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Перевести";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // cbDestCurrency
+            // 
+            this.cbDestCurrency.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDestCurrency.FormattingEnabled = true;
+            this.cbDestCurrency.Location = new System.Drawing.Point(276, 100);
+            this.cbDestCurrency.Name = "cbDestCurrency";
+            this.cbDestCurrency.Size = new System.Drawing.Size(177, 21);
+            this.cbDestCurrency.TabIndex = 2;
+            // 
+            // cbSourceCurrency
+            // 
+            this.cbSourceCurrency.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSourceCurrency.FormattingEnabled = true;
+            this.cbSourceCurrency.Location = new System.Drawing.Point(64, 100);
+            this.cbSourceCurrency.Name = "cbSourceCurrency";
+            this.cbSourceCurrency.Size = new System.Drawing.Size(177, 21);
+            this.cbSourceCurrency.TabIndex = 3;
+            // 
+            // tbDestCurrency
+            // 
+            this.tbDestCurrency.Location = new System.Drawing.Point(276, 74);
+            this.tbDestCurrency.Name = "tbDestCurrency";
+            this.tbDestCurrency.ReadOnly = true;
+            this.tbDestCurrency.Size = new System.Drawing.Size(177, 20);
+            this.tbDestCurrency.TabIndex = 4;
+            this.tbDestCurrency.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbDestCurrency_KeyPress);
+            // 
+            // tbSourceCurrency
+            // 
+            this.tbSourceCurrency.Location = new System.Drawing.Point(63, 74);
+            this.tbSourceCurrency.Name = "tbSourceCurrency";
+            this.tbSourceCurrency.Size = new System.Drawing.Size(178, 20);
+            this.tbSourceCurrency.TabIndex = 5;
+            this.tbSourceCurrency.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbDestCurrency_KeyPress);
+            // 
+            // lblConnected
+            // 
+            this.lblConnected.AutoSize = true;
+            this.lblConnected.Location = new System.Drawing.Point(12, 9);
+            this.lblConnected.Name = "lblConnected";
+            this.lblConnected.Size = new System.Drawing.Size(140, 13);
+            this.lblConnected.TabIndex = 6;
+            this.lblConnected.Text = "Нет подключения к НБРБ.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 173);
+            this.Controls.Add(this.lblConnected);
+            this.Controls.Add(this.tbSourceCurrency);
+            this.Controls.Add(this.tbDestCurrency);
+            this.Controls.Add(this.cbSourceCurrency);
+            this.Controls.Add(this.cbDestCurrency);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnReconnect);
+            this.Name = "MainForm";
+            this.Text = "Currency сonverter";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReconnect;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.ComboBox cbDestCurrency;
+        private System.Windows.Forms.ComboBox cbSourceCurrency;
+        private System.Windows.Forms.TextBox tbDestCurrency;
+        private System.Windows.Forms.TextBox tbSourceCurrency;
+        private System.Windows.Forms.Label lblConnected;
+    }
+}
+
---
+++ Conversion/MainForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using NbrbAPI.Models;
+using Newtonsoft.Json;
+
+namespace Conversion
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();  
+        }
+
+        private Rate[] rate;
+
+        private bool getCurrencies()
+        {
+            string rateRequest = "http://www.nbrb.by/api/exrates/rates?periodicity=0";
+
+            StreamReader stream = null;
+            try
+            {
+                // Get currencies
+                System.Net.HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(rateRequest);
+                System.Net.HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+                string response;
+                stream = new StreamReader(resp.GetResponseStream(), Encoding.UTF8);
+                response = stream.ReadToEnd();
+               
+                Rate[] temp = JsonConvert.DeserializeObject<Rate[]>(response);
+                rate = new Rate[temp.Length + 1];
+                temp.CopyTo(rate, 0);
+
+                rate[rate.Length - 1] = new Rate { Cur_ID = 0, Cur_Scale = 1, Cur_OfficialRate = 1M, Cur_Name = "Белорусский рубль" };
+
+                return true;
+            }
+            catch (WebException)
+            {
+                MessageBox.Show("Ошибка подключения к НБРБ. Проверьте Ваше интернет соединение.");
+                return false;
+            }
+            catch (JsonException)
+            {
+                MessageBox.Show("Ошибка десериализации.");
+                return false;
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Ошибка чтения ответа от НБРБ.");
+                return false;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+             }
+            
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (getCurrencies())
+            {
+                initializeComponents();
+            }
+        }
+
+        private void initializeComponents()
+        {
+            foreach (Rate currency in rate)
+            {
+                cbSourceCurrency.Items.Add(currency.Cur_Name);
+                cbDestCurrency.Items.Add(currency.Cur_Name);
+            }
+
+            btnReconnect.Enabled = false;
+            btnConvert.Enabled = true;
+            lblConnected.Text = "Подключение установлено.";
+
+            if (cbSourceCurrency.Items.Count > 0)
+            {
+                cbSourceCurrency.SelectedIndex = 0;
+                cbDestCurrency.SelectedIndex = 0;
+            }
+        }
+
+        private void btnReconnect_Click(object sender, EventArgs e)
+        {
+            if (getCurrencies())
+            {
+                initializeComponents();
+            }
+        }
+
+        private void tbDestCurrency_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!char.IsDigit(number) && number != 8 && number != 46) // цифры, клавиша BackSpace и точка
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            decimal src;
+
+            try
+            {
+                CultureInfo cultures = new CultureInfo("en-US");
+
+                src = Convert.ToDecimal(tbSourceCurrency.Text, cultures);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неверный формат чисел.");
+                return;
+            }
+
+            Rate srcRate = rate[cbSourceCurrency.SelectedIndex];
+            Rate destRate = rate[cbDestCurrency.SelectedIndex];
+            decimal? result;
+
+            try
+            {
+                result = (src / srcRate.Cur_Scale * srcRate.Cur_OfficialRate) / destRate.Cur_OfficialRate * destRate.Cur_Scale;
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("В ходе расчёта произошло деление на ноль.");
+                return;
+            }
+            tbDestCurrency.Text = string.Format("{0:F2}", result);
+        }
+    }
+}
